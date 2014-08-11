8b1c886669397321a0057075f4c30245d0a7116c
kairusso <kairusso@kais-mbp.cityhall.boston.cob>
2014-08-11 12:49:22 -0400

    firstTry Homepage

---
+++ myHome.js
@@ -0,0 +1,49 @@
+	
+
+	$(document).ready( function() {
+
+		
+
+		$("input#search").keyup(function(event){
+    		if(event.keyCode == 13){
+        		$("input#submit").click();
+    		}
+		});
+
+		
+
+		$("input#submit").click( function() {
+
+			var add = $("input#search").val().trim();
+			add = add.replace(',', '');
+			add = add.replace('.', '');
+			
+			var parts = add.split(" ");
+			
+			console.log(add);
+
+			if (add === '', isNaN(parts[0])) {
+				alert("Please fill the box correctly");
+
+			$('div.header').children().each( function() {
+				$(this).val('');
+			});
+		}
+		else {
+
+
+			var url = "http://localhost:8888/search_final.html?parameter=" + add;
+			
+
+			window.open(url,"_self")
+
+			
+			w.getElementById('search').innerHTML = add;
+
+			.fail( function() {
+				console.log("Error retrieving server query");
+			});
+
+		}
+		});
+	});
